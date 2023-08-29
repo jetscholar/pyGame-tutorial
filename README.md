6f6a99616e9a40e94e9f8ec4c5915062ce089cc7
Michael Zampogna <mrz@jetscholar.com>
2023-08-30 06:49:36 +1000

    add venv

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 [Get Started in Pygame in 10 minutes!](https://www.youtube.com/watch?v=y9VG3Pztok8)
 
 [Russ's Website](http://www.codingwithruss.com/pygame/get-started-in-pygame-in-10-minutes/)
+
+## Work Log
+
+Date | Commit Description | Version
+-----|--------------------|--------
+29.08.23 | Initial Commit | 0.1.0
+30.08.23 | added venv | 0.1.1
--- main.py
+++ main.py
@@ -5,15 +5,38 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+# define the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Gam Loop
+# setup a player 
+player = pygame.Rect((300, 250, 50, 50))
+
+# Game Loop
 run = True
 while run:
     
+    # refresh screen
+    screen.fill((0, 0, 0))
+    
+    # draw a player in red
+    pygame.draw.rect(screen, (255, 0, 0), player)
+    
+    # controls
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0) # move left (x, y)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0) # move right
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1) # move up
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1) # move down
     # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
+    # screen update
+    pygame.display.update()
 
 pygame.quit()
